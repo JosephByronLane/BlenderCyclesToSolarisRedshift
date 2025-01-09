c1f3ceef33cf11d871fdc23149ff15d8c003cc9b
Joseph Byron Lane <24197141@modelo.edu.mx>
2025-01-09 00:32:14 -0600

    added more error checking to houdini script

--- houdini/script.py
+++ houdini/script.py
@@ -26,14 +26,18 @@ def import_rsir_json(filepath, matlibNode=None):
     
     #before we do anything we delete the existing standardMaterial and redshift_usd_material nodes
     #since we will be creating our own nodes
-    existingStandardMaterialNode = parent_node.node("StandardMaterial1")
-    exitinggRedshiftUsdMaterialNode = parent_node.node("redshift_usd_material1")
 
-    if existingStandardMaterialNode:            
-        existingStandardMaterialNode.destroy()
+    try:
+        existingStandardMaterialNode = parent_node.node("StandardMaterial1")
+        exitinggRedshiftUsdMaterialNode = parent_node.node("redshift_usd_material1")
+        if existingStandardMaterialNode:            
+            existingStandardMaterialNode.destroy()
 
-    if exitinggRedshiftUsdMaterialNode:
-        exitinggRedshiftUsdMaterialNode.destroy()
+        if exitinggRedshiftUsdMaterialNode:
+            exitinggRedshiftUsdMaterialNode.destroy()
+
+    except Exception as e:
+        print(f"Error deleting existing nodes: {e}")
 
 
     for graph in rsirGraphs:
@@ -41,39 +45,46 @@ def import_rsir_json(filepath, matlibNode=None):
         #first we create the RS Nodes based on the RSIRGraph children
         
         children = graph["children"]
+        print("******************************************************")
+        print("Creating individual graphs")   
 
         for child in children:
             nodeType = child["type"]
             nodeName = child["id"]
             nodeProps = child["properties"]
-            print("-----------------------------------------------------")
-            print(f"Creating node: {nodeName} of type {nodeType}")
+            print("-----------------------------------------------------------")
+            print(f"Creating node: {nodeName} of type {nodeType} on graph {graph.uId}")
             createdNode = parent_node.createNode(nodeType, node_name=nodeName)
             for name, value in nodeProps.items():
                 try:
+                    print(f"Setting parameter '{name}' to '{value}' on node '{nodeName}'")
                     if isinstance(value, (list, tuple)) and len(value) > 1:
                         parm = createdNode.parmTuple(name)
-                        if parm:
+                        if parm is not None:
                             parm.set(value)
                         else:
-                            print(f"Parameter '{name}' not found on node '{nodeName}'.")
+                            raise Exception(f"Tuple parameter '{name}' not found on node '{nodeName}'.")
+
                     else:
                         parm = createdNode.parm(name)
-                        if parm:
+                        if parm is not None:
                             parm.set(value)
                         else:
-                            print(f"Parameter '{name}' not found on node '{nodeName}'.")
-                            
+                            raise Exception(f"Float parameter '{name}' not found on node '{nodeName}'.")
+
                 except Exception as e:
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                     print(f"Failed to set parameter '{name}' on node '{nodeName}': {e}")
-            
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
         #after graph the child nodes, we wire the individual graphs up
         internalConnections = graph["internalConnections"]
-        print("************")
-        print("Connecting nodes")   
+        print("******************************************************")
+        print("Wiring individual graphs")   
         
         for connection in internalConnections:
             try:    
+                print("##############################################################")
                 nodeMakingConnectionName = connection.split(":")[0]
                 nodeMakingConnectionOutoutSocker = connection.split(":")[1]
                 print(f"Node making connection: {nodeMakingConnectionName} and output socket: {nodeMakingConnectionOutoutSocker} ")
@@ -85,6 +96,7 @@ def import_rsir_json(filepath, matlibNode=None):
 
                 #TODO: use pyside to make panels to check if user wants to import into stage or /mat/
                 nodeTakingConnection = hou.node(f"/stage/{matlibNode.name()}/{matname}/{nodeTakingConnectionName}")
+
                 if nodeTakingConnection is None:
                     raise Exception(f"Node {nodeTakingConnectionName} not found in the graph")
 
@@ -93,15 +105,17 @@ def import_rsir_json(filepath, matlibNode=None):
 
 
                 #setNamedInput("refl_roughness", inputNode, "outColor")
-                print(f"{nodeTakingConnection.name()}, {nodeTakingConnectingInputSocket}, {nodeMakingInputConnection.name()}, {nodeMakingConnectionOutoutSocker} ")
+                print(f"Data info:{nodeTakingConnection.name()}.setNamedInput( {nodeTakingConnectingInputSocket}, {nodeMakingInputConnection.name()}, {nodeMakingConnectionOutoutSocker}) ")
                 nodeTakingConnection.setNamedInput(nodeTakingConnectingInputSocket, nodeMakingInputConnection, nodeMakingConnectionOutoutSocker)
                 print("#########")
             except Exception as e:
+
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 print(f"Error connecting nodes: {e}")
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-
-        print("************")
-        print("Connecting graphs")
+        print("******************************************************")
+        print("Connecting graphs between each other")
         #now we wire the connections between graphs
     for graph in rsirGraphs:
         inputConnections = graph["inputConnections"]
@@ -112,7 +126,7 @@ def import_rsir_json(filepath, matlibNode=None):
             try:    
                 nodeMakingConnectionName = connection.split(":")[0]
                 nodeMakingConnectionInputSocketName = connection.split(":")[1]
-
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
                 print(f"Node making input: {nodeMakingConnectionName} and input socket: {nodeMakingConnectionInputSocketName} ")
 
                 nodeMakingConnection = hou.node(f"/stage/{matlibNode.name()}/{matname}/{nodeMakingConnectionName}")
@@ -134,27 +148,33 @@ def import_rsir_json(filepath, matlibNode=None):
                 nodeTakingConnection.setNamedInput(nodeTakingConnectionSocketName, nodeMakingConnection, nodeMakingConnectionInputSocketName)
                 
             except Exception as e:
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 print(f"Error connecting graphs: {e}")
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 
+    try:
+        #now we connect the suboutput
+        subOutputNode = hou.node(f"/stage/{matlibNode.name()}/{matname}/suboutput1")
+        if subOutputNode is None:
+            raise Exception(f"Node suboutput1 not found in the graph")
+        
+        #NOTE if we want to support principled material blending we need to programatically set the output node rather than just assuming its the first standard material
+        # to do this we could simply find the MaterialLayer or MaterialBlender  (depending on the approach we take) with the highest number at the end of their name.
+        standardMaterialNode = hou.node(f"/stage/{matlibNode.name()}/{matname}/redshift_usd_material1")
+        if standardMaterialNode is None:
+            raise Exception(f"Node StandardMaterial1 not found in the graph")
+        
 
-    #now we connect the suboutput
-    subOutputNode = hou.node(f"/stage/{matlibNode.name()}/{matname}/suboutput1")
-    if subOutputNode is None:
-        raise Exception(f"Node suboutput1 not found in the graph")
-    
-    #NOTE if we want to support principled material blending we need to programatically set the output node rather than just assuming its the first standard material
-    # to do this we could simply find the MaterialLayer or MaterialBlender  (depending on the approach we take) with the highest number at the end of their name.
-    standardMaterialNode = hou.node(f"/stage/{matlibNode.name()}/{matname}/redshift_usd_material1")
-    if standardMaterialNode is None:
-        raise Exception(f"Node StandardMaterial1 not found in the graph")
-    
+        print(f"{subOutputNode.name()}, {standardMaterialNode.name()}")
+        subOutputNode.setInput(0,standardMaterialNode, 0)
 
-    print(f"{subOutputNode.name()}, {standardMaterialNode.name()}")
-    subOutputNode.setInput(0,standardMaterialNode, 0)
-
-    parent_node.layoutChildren()
-    matlibNode.layoutChildren()
+        parent_node.layoutChildren()
+        matlibNode.layoutChildren()
+    except Exception as e:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(f"Error connecting suboutput: {e}")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 def has_hidden_attribute(filepath):
     return bool(os.stat(filepath).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
@@ -182,13 +202,16 @@ def autoDetectFolder(autoDetectDrive='H:/'):
         raise Exception(f"Error during auto-detection: {e}")
 
 def findAllJson(projectDirectory):
-    foundFiles=[]
-    for root, dirs, files in os.walk(projectDirectory + '/tex/'):
-    # select file name
-        for file in files:
-            # check the extension of files
-            if file.endswith('.json'):
-                foundFiles.append(os.path.join(root, file))
+    try:
+        foundFiles=[]
+        for root, dirs, files in os.walk(projectDirectory + '/tex/'):
+        # select file name
+            for file in files:
+                # check the extension of files
+                if file.endswith('.json'):
+                    foundFiles.append(os.path.join(root, file))
+    except Exception as e:
+        raise Exception(f"Error during JSON search: {e}")
 
     return foundFiles
 
