32bf2509c7a13a12e2ff64959f025289312fd1f7
Joseph Byron Lane <24197141@modelo.edu.mx>
2025-01-09 20:33:42 -0600

    changed error handling in material parser

--- operators/materialParser.py
+++ operators/materialParser.py
@@ -22,14 +22,13 @@ class RFXUTILS_OT_MaterialParser(bpy.types.Operator):
         scn.custom.clear()
         scn.custom_index = 0
 
-    def addErrorsToCustomList(self, errors, misc):
+    def addErrorsToCustomList(self, errorString, material):
         """Adds each string in `errors` as a new item to the custom list."""
         scn = bpy.context.scene
-        for err, mat in zip(errors, misc):
-            new_item = scn.custom.add()
-            new_item.mat = mat
-            new_item.name = err
-            new_item.coll_id = len(scn.custom)  # or some other logic for coll_id
+        new_item = scn.custom.add()
+        new_item.mat = material
+        new_item.name = errorString
+        new_item.coll_id = len(scn.custom)  # or some other logic for coll_id
         scn.custom_index = len(scn.custom) - 1  # Make sure the last one is selected
 
     def execute(self, context):
@@ -40,12 +39,13 @@ class RFXUTILS_OT_MaterialParser(bpy.types.Operator):
             return {'CANCELLED'}
 
         uniqueId = ""
-
-
+        allErrors = []
+        parsedNodes = []
         #used to verify if a material has already been exported
         alreadyExportedMmaterials = set()
 
         for obj in selectedObjects:
+
             if obj.type != 'MESH':
                 continue
             for mat in obj.data.materials:
@@ -56,11 +56,10 @@ class RFXUTILS_OT_MaterialParser(bpy.types.Operator):
                     RSIRGraphs= []
                     self.clearErrorsFromCustomList()
                     errors = []
-                    nodesToParse = mat.node_tree.nodes
 
+                    #TODO: rewrite error handling cause its really finnicky and not very good
                     #  nodes
                     for node in mat.node_tree.nodes:
-                        totalErrors = []
                         errors = []
 
                         nodeRegistry = getRegistry(node.bl_idname)
@@ -74,21 +73,14 @@ class RFXUTILS_OT_MaterialParser(bpy.types.Operator):
                             RSIRGraphs.append(nodeFunction) 
                             
                             if errors:           
-                                print("Errors founaaaaad")
-                                for _ in errors:
-                                    print("Iterating over error")
-                                    misc.append(mat.name)     
+                                for error in errors:
+                                    allErrors.append(error)
+                                    print(f"Found error in node {node.name}: {error}")
+                                    self.addErrorsToCustomList(error, mat.name)        
 
-                                print("Errors found within material", mat.name)
-                                print(errors)
-                                print(f"total errors before extended", totalErrors)
-                                totalErrors.extend(errors)
-
-                                print(f"total errors after extended", totalErrors)
-
-                                self.addErrorsToCustomList(totalErrors, misc)                         
                                 continue
-
+                            print(f"Node {node.name} parsed successfully")
+                            parsedNodes.append(node.name)
                             uniqueId = mat.name  #uuid because why not lmao       
                             GLOBAL_DATA_STORE[uniqueId] = {
                                 "mat": mat,
@@ -96,79 +88,86 @@ class RFXUTILS_OT_MaterialParser(bpy.types.Operator):
                             }
 
                         else:
-                            totalErrors = []
-                            totalErrors.extend([f"Node {node.bl_idname} not supported "])
-                            misc.append(mat.name)
-
-                            self.addErrorsToCustomList(totalErrors, misc)                         
-
-                            print("errors found with not supported node")
-                            print("total errors before extended", totalErrors)
-                            print(totalErrors)
-                            print("total errors after extended", totalErrors)
-
-                            print(misc)
+                            error = f"Node {node.bl_idname} not supported "
+                            self.addErrorsToCustomList(error, mat.name)  
+                            allErrors.append(error)
                             continue
 
                     # now we fil out the inputConnections field
                     for node in mat.node_tree.nodes:                        
+                        if node.name in parsedNodes:
+                            print("Parsing connections for node", node.name)
+                            #since RSIRGraphs is a list of  custom objects and not data, we cant just use the .index() function, we need to manually search it
+                            currentNodeRSIRGraph = None
+                            for rsirGraph in RSIRGraphs:
+                                if rsirGraph.uId == node.name:
+                                    currentNodeRSIRGraph = rsirGraph
+                                    break
+                                    
+                            if currentNodeRSIRGraph is None:
+                                self.report({'ERROR'}, "There was an error hooking up node conections: Couldnt find current node's RSIRGraph")
+                                return {'CANCELLED'}
 
-                        #since RSIRGraphs is a list of  custom objects and not data, we cant just use the .index() function, we need to manually search it
-                        currentNodeRSIRGraph = None
-                        for rsirGraph in RSIRGraphs:
-                            if rsirGraph.uId == node.name:
-                                currentNodeRSIRGraph = rsirGraph
-                                break
-                                
-                        if currentNodeRSIRGraph is None:
-                            self.report({'ERROR'}, "There was an error hooking up node conections: Couldnt find current node's RSIRGraph")
-                            return {'CANCELLED'}
+                            
+                            for input_socket in node.inputs:
+                                if input_socket.is_linked:
+                                    try:
+                                        # These are in RS nodes, since they're pased to the houdini parser.
+                                        # 
+                                        # To find these connectors, its going to query the blender node and ask:
+                                        # "Hey, I see you are connected to my ShaderNodeMix input A through your Texture1 color output,
+                                        # My actual ShaderNodeMix input A is actually inboundConnectors["ShaderNodeMix:A"] (RScolorMix1:input1), 
+                                        # and I see your Texture1 color output is actually inputNode.outboundConnectors["Texture1:Color"] (RSColorMaker1:outColor)
+                                        # and so it writes them for the parser.
+                                        #
+                                        # "RSMathRange1:input" : "RSColorSplitter1:outA",
 
-                        
-                        for input_socket in node.inputs:
-                            if input_socket.is_linked:
-                                # These are in RS nodes, since they're pased to the houdini parser.
-                                # 
-                                # To find these connectors, its going to query the blender node and ask:
-                                # "Hey, I see you are connected to my ShaderNodeMix input A through your Texture1 color output,
-                                # My actual ShaderNodeMix input A is actually inboundConnectors["ShaderNodeMix:A"] (RScolorMix1:input1), 
-                                # and I see your Texture1 color output is actually inputNode.outboundConnectors["Texture1:Color"] (RSColorMaker1:outColor)
-                                # and so it writes them for the parser.
-                                #
-                                # "RSMathRange1:input" : "RSColorSplitter1:outA",
+                                        connectingNode = input_socket.links[0].from_node     
 
-                                connectingNode = input_socket.links[0].from_node     
+                                        connectingNodeRSIRGraph = None
+                                        for rsirGraph in RSIRGraphs:
+                                            if rsirGraph.uId == connectingNode.name:
+                                                if rsirGraph.uId in parsedNodes:
+                                                    connectingNodeRSIRGraph = rsirGraph
+                                                    break
 
-                                connectingNodeRSIRGraph = None
-                                for rsirGraph in RSIRGraphs:
-                                    if rsirGraph.uId == connectingNode.name:
-                                        connectingNodeRSIRGraph = rsirGraph
-                                        break
+                                        #if the RSIRGraph is not found, in the connecting inputs, means its graph was never greated, and thus never parsed. AKA isn't supported
+                                        if connectingNodeRSIRGraph is None:
 
-                                if connectingNodeRSIRGraph is None:
-                                    self.report({'ERROR'}, "There was an error hooking up node conections: Couldnt find connecting node's RSIRGraph")
-                                    return {'CANCELLED'}
+                                            #these errors can be ignored, as they are already reported in the node parsing stage
+
+                                            # self.addErrorsToCustomList(f"Unsuported node {connectingNode.name} found. Will be ignored", mat.name)  
+                                            # allErrors.append(f"Unsuported node {connectingNode.name} found. Will be ignored")
+                                            continue
 
 
-                                connectingGraphOutboundConnectors = connectingNodeRSIRGraph.outboundConnectors                                    
-                                
-                                #we get the connectors to do the  blender -> redshift name translation
-                                currentGraphInboundConnectors = currentNodeRSIRGraph.inboundConnectors
-                                currentGraphInputConnections = currentNodeRSIRGraph.inputConnections
+                                        connectingGraphOutboundConnectors = connectingNodeRSIRGraph.outboundConnectors                                    
+                                        
+                                        #we get the connectors to do the  blender -> redshift name translation
+                                        currentGraphInboundConnectors = currentNodeRSIRGraph.inboundConnectors
+                                        currentGraphInputConnections = currentNodeRSIRGraph.inputConnections
 
-                                currentNodeBlId = node.bl_idname
-                                currentNodeConnectedSocketName = input_socket.name
-                                print(currentGraphInboundConnectors.keys())
+                                        currentNodeBlId = node.bl_idname
+                                        currentNodeConnectedSocketName = input_socket.name
 
-                                currentNodeRedshiftTranslatedSocket = currentGraphInboundConnectors[f"{currentNodeBlId}:{currentNodeConnectedSocketName}"]
+                                        currentNodeRedshiftTranslatedSocket = currentGraphInboundConnectors.get(f"{currentNodeBlId}:{currentNodeConnectedSocketName}")
+                                        
+                                        connectingNodeBlId = connectingNode.bl_idname
+                                        inputNodeSocketName = input_socket.links[0].from_socket.name
+                                        inputNodeRedshiftTranslatedSocket = connectingGraphOutboundConnectors.get(f"{connectingNodeBlId}:{inputNodeSocketName}")
 
-                                connectingNodeBlId = connectingNode.bl_idname
-                                inputNodeSocketName = input_socket.links[0].from_socket.name
-                                inputNodeRedshiftTranslatedSocket = connectingGraphOutboundConnectors[f"{connectingNodeBlId}:{inputNodeSocketName}"]
+                                        if currentNodeRedshiftTranslatedSocket is None or inputNodeRedshiftTranslatedSocket is None:
+                                            error = f"Error hooking up node connections: Couldnt find redshift translated socket for {currentNodeBlId}"
+                                            self.addErrorsToCustomList(error, mat.name)  
+                                            allErrors.append(error)
+                                        else:
+                                            currentGraphInputConnections[inputNodeRedshiftTranslatedSocket] = f"{currentNodeRedshiftTranslatedSocket}"
+                                        continue
+                                    except Exception as e:
+                                        self.report({'ERROR'}, f"Error hooking up node connections: {e}")
+                                        return {'CANCELLED'}
 
-                                currentGraphInputConnections[inputNodeRedshiftTranslatedSocket] = f"{currentNodeRedshiftTranslatedSocket}"
-
-                    if totalErrors:
+                    if allErrors:
                         print("Errors found")
                         bpy.ops.rfxutils.call_popup('INVOKE_DEFAULT', key=uniqueId)
 
