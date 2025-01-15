c3fb863357675a9d1f65561c41642d1f8c11eb9f
Joseph Byron Lane <24197141@modelo.edu.mx>
2025-01-15 00:49:19 -0600

    added button to disconnect all specular inputs

--- operators/disconnectSpecular.py
+++ operators/disconnectSpecular.py
@@ -11,7 +11,22 @@ class RFX_OT_DisconnectSpecular(bpy.types.Operator):
         return True
 
     def execute(self, context):
-        
+        selectedObjects = bpy.context.selected_objects
+
+        parsedMaterials = []
+        for object in selectedObjects:
+            if object.type == "MESH":
+                for mat in object.data.materials:
+                    if mat and mat.name not in parsedMaterials:
+                        nodeTree = mat.node_tree
+                        for node in nodeTree.nodes:
+                            if node.bl_idname == "ShaderNodeBsdfPrincipled":
+                                for input in node.inputs:
+                                    if input.name == "Specular IOR Level" or input.name == "Specular Tint" or input.name == "Anisotropic" or input.name == "Anisotropic Rotation" or input.name == "Tangent":
+                                        for link in input.links:
+                                            nodeTree.links.remove(link)
+
+                                      
         return {"FINISHED"}
 
 
