1395699baa5e1ad7e05c4a507800261ed9043ef3
Joseph Byron Lane <24197141@modelo.edu.mx>
2025-01-16 03:07:15 -0600

    added propriety for separate collider + gear gltf

--- proprieties/glftImportSettings.py
+++ proprieties/glftImportSettings.py
@@ -6,5 +6,10 @@ def register():
         description="Auto detects the character GLTF file from the Houdini Project folder.",
         default=False
     )
+    bpy.types.Scene.separate_col_gear_gltf = bpy.props.BoolProperty(
+        name="Separate Gear and Collider GLTF",
+        description="Tells the importer wether you have separate collider + gear GLTF or a single file.",
+        default=False
+    )
 def unregister():
     del bpy.types.Scene.auto_detect_gltf
