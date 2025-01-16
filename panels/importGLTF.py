d3d7ac4e7ceafd6f85865769ba63b1fd1c986bc7
Joseph Byron Lane <24197141@modelo.edu.mx>
2025-01-15 23:31:23 -0600

    removed fix ss on export since we made a dedicated button for it

---
+++ panels/importGLTF.py
@@ -0,0 +1,27 @@
+import bpy
+from bpy.types import Panel, Operator
+
+
+class VIEW3D_PT_MaterialExporter(Panel):
+    """TODO: add description"""
+    bl_label = "Material Parser"
+    bl_idname = "RFXUTILS_PT_panel"
+    bl_space_type = 'VIEW_3D'     
+    bl_region_type = 'UI'         
+    bl_category = "RuneFX Utils" 
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.prop(context.scene , "ignore_invert_nodes", text="Ignore Invert Nodes")
+        layout.prop(context.scene , "move_textures_over", text="Move Textures Over")
+        layout.prop(context.scene , "include_osl_shaders", text="Allow OSL Shaders")
+        layout.prop(context.scene , "move_osl_shaders", text="Move OSL Shaders")
+
+        layout.operator("rfxutils.material_parser", text="Export Materials")
+
+def register():
+    bpy.utils.register_class(VIEW3D_PT_MaterialExporter)
+
+def unregister():
+    bpy.utils.unregister_class(VIEW3D_PT_MaterialExporter)
